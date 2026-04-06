6ead112b5afad9cef272d31bdb26738b6275ca41
shamil <for.photos.only001@gmail.com>
2026-04-06 23:21:28 +0530

    urls fixed

--- getProcess/pgm.ps1
+++ getProcess/pgm.ps1
@@ -1,7 +1,7 @@
 Write-Host "welcome to deep work"
 Write-Host "Starting env"
 
-Start-Process "code"
+Start-Process "code $env:USERPROFILE\Downloads"
 Start-Process "http://localhost:3000"
-Start-Process "https://github/shamil-tp"
+Start-Process "https://github.com/shamil-tp"
 Start-Process "https://gemini.google.com/app"
