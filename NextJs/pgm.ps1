6415ec8867ad920535be1a461afe18ea44e916e6
shamil <for.photos.only001@gmail.com>
2026-04-07 22:06:34 +0530

    git commands with ps1

---
+++ NextJs/pgm.ps1
@@ -0,0 +1,11 @@
+Write-Host "hello this is automation for Next Js"
+Set-Location ../
+$result = git status
+
+if ($result) {
+    <# Action to perform if the condition is true #>
+    git add .
+    git commit -m "new commit"
+}else {
+    Write-Host "noting to commit"
+}
