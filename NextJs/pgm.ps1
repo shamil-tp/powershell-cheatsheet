27fbfe2a23e0c89a4999c695636c54a1a534d063
shamil <for.photos.only001@gmail.com>
2026-04-07 22:28:53 +0530

    completed simple git automation

--- NextJs/pgm.ps1
+++ NextJs/pgm.ps1
@@ -2,11 +2,26 @@ Write-Host "hello this is automation for Next Js"
 Set-Location ../
 $result = git status
 
-if ($result) {
-    <# Action to perform if the condition is true #>
+if($result -match "nothing to commit"){
+    Write-Host "Sir I found Nthing to commit please make any changes to commit"
+}else{
+    git status
+    Start-Sleep -Seconds 1
+    Write-Host "Staging Changes for commiting" -ForegroundColor Yellow
     git add .
-    git commit -m "new commit"
-}else {
-    Write-Host "noting to commit"
+    
+    Start-Sleep -Seconds 1
+    Write-Host "Commiting Staged Changes" -ForegroundColor Yellow
+    $message = Read-Host "Please Enter A Message for commiting: " -ForegroundColor Blue
+    git commit -m "$message"
+    
+    $query = Read-Host "Sir Do you want to push this to the remote Now ? [Y/N]"
+
+    if($query -eq "Y"){
+        Start-Sleep -Seconds 1
+        Write-Host "Pushing to Remote" -ForegroundColor Yellow
+        git push -u origin main
+    }
+    
 }
 Set-Location .\NextJs
