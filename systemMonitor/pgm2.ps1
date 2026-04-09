0c08a3514480b92d4a4e3b6ad74cb7f3dab0d7ae
shamil <for.photos.only001@gmail.com>
2026-04-09 23:43:54 +0530

    Reduced Health monit

---
+++ systemMonitor/pgm2.ps1
@@ -0,0 +1,6 @@
+Write-Host "Hello Sir This Your Reduced System Health Info"
+Write-Host " "
+Write-Host "=============================================="
+Write-Host "=================Health Monit================="
+Write-Host "=============================================="
+
