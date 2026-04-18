7f509b256966dc446001ad7c3539d27895f1c0ad
shamil <for.photos.only001@gmail.com>
2026-04-18 23:51:46 +0530

    network connections

--- connections/wifi.ps1
+++ connections/wifi.ps1
@@ -1,2 +1,5 @@
 Get-NetAdapter
-Disable-NetAdapter -Name "Wi-Fi" -Confirm:$false
+# Disable-NetAdapter -Name "Wi-Fi" -Confirm:$false
+Get-NetConnectionProfile
+netsh wlan show profile name="NETWORK_NAME" key=clear
+netsh wlan connect name="NETWORK_NAME"
